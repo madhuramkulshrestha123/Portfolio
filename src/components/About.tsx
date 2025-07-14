45c2c7f3ca20cdf6b02f5aaf17da207be0a93630
ojaswii <madhuramkulshrestha447@gmail.com>
2025-07-15 01:08:15 +0530

    Initial commit

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          Hey, I’m Madhuram Kulshrestha, I work on backend systems, full-stack apps, and occasionally build with machine learning. I enjoy solving real-world problems with code and love working on scalable, efficient solutions.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Career.tsx
@@ -0,0 +1,63 @@
+import "./styles/Career.css";
+
+const Career = () => {
+  return (
+    <div className="career-section section-container">
+      <div className="career-container">
+        <h2>
+          My career <span>&</span>
+          <br /> experience
+        </h2>
+        <div className="career-info">
+          <div className="career-timeline">
+            <div className="career-dot"></div>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Data Science Intern</h4>
+                <h5>Sabudh Foundation(STPI)</h5>
+              </div>
+              <h3>Jan'25</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              labore sit non ipsum temporibus quidem, deserunt eaque officiis
+              mollitia ratione suscipit repellat.
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Software Developer Intern</h4>
+                <h5>FRIHBI ENTERPRISES PVT. LTD.</h5>
+              </div>
+              <h3>July'25</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              labore sit non ipsum temporibus quidem, deserunt eaque officiis
+              mollitia ratione suscipit repellat.
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Soon</h4>
+                <h5>Your Organization</h5>
+              </div>
+              <h3>NOW??</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              labore sit non ipsum temporibus quidem, deserunt eaque officiis
+              mollitia ratione suscipit repellat.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Career;
